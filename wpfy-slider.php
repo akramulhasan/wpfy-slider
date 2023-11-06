b04e8b68a4e3176d6968b9b6ac57c547b7a8225d
Akramul Hasan <lighthouse8048@gmail.com>
2023-11-06 09:33:20 +0600

    Initial Commit

---
+++ wpfy-slider.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * WPFY Slider
+ *
+ * @package           wpfyslider
+ * @author            wpfy
+ * @license           GPL-2.0-or-later
+ *
+ * @wordpress-plugin
+ * Plugin Name:       WPFY Slider
+ * Plugin URI:        https://akramulhasan.com/wpfy-slider
+ * Description:       Description of the plugin.
+ * Version:           1.0.0
+ * Requires at least: 5.2
+ * Requires PHP:      7.2
+ * Author:            wpfy
+ * Author URI:        https://akramulhasan.com
+ * Text Domain:       wpfy-slider
+ * License:           GPL v2 or later
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Update URI:        https://akramulhasan.com/my-plugin
+ */
+
+if( ! defined( 'ABSPATH' ) ){
+    exit;
+}
