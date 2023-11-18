413d81cb1bdc343d60cbea855b353f5cadbc0e01
Akramul Hasan <lighthouse8048@gmail.com>
2023-11-18 23:00:40 +0600

    meta box added

--- post-types/class.wpfy-slider-cpt.php
+++ post-types/class.wpfy-slider-cpt.php
@@ -4,6 +4,8 @@ if(!class_exists('WPFY_SLIDER_CPT')){
     class WPFY_SLIDER_CPT {
         function __construct(){
             add_action('init', array($this, 'create_slider_post_type'));
+            add_action('add_meta_boxes', array($this, 'add_meta_boxes'));
+            add_action('save_post', array($this, 'save_post'), 10, 2);
         }
 
         public function create_slider_post_type(){
@@ -30,6 +32,35 @@ if(!class_exists('WPFY_SLIDER_CPT')){
                 'menu_icon' => 'dashicons-images-alt2'
             ));
 
+        } // End create_slider_post_type
+
+        public function add_meta_boxes(){
+            add_meta_box( 
+                'wpfy_slider_meta_box', 
+                'Link Options',
+                array($this, 'add_inner_beta_boxes'),
+                'wpfy-slider',
+                'normal', 
+                'high'
+               
+            );
         }
+
+        public function add_inner_beta_boxes($post){
+            require_once(WPFY_SLIDER_PATH . 'views/wpfy-slider-metabox.php');
+        }
+
+        public function save_post($post_id){
+            //if( isset( $_POST['action'] ) && $_POST['action'] == 'editpost' ){
+                $old_link_text = get_post_meta($post_id, 'wpfy_slider_link_text', true);
+                $new_link_text = $_POST['wpfy_slider_link_text'];
+                $old_link_url = get_post_meta($post_id, 'wpfy_slider_link_url', true);
+                $new_link_url = $_POST['wpfy_slider_link_url'];
+
+                update_post_meta( $post_id, 'wpfy_slider_link_text', $new_link_text, $old_link_text );
+                update_post_meta( $post_id, 'wpfy_slider_link_url', $new_link_url, $old_link_url );
+            //}
+        }
+
     }
 }
---
+++ views/wpfy-slider-metabox.php
@@ -0,0 +1,33 @@
+<table class="form-table mv-slider-metabox"> 
+    <tr>
+        <th>
+            <label for="wpfy_slider_link_text">Link Text</label>
+        </th>
+        <td>
+            <input 
+                type="text" 
+                name="wpfy_slider_link_text" 
+                id="wpfy_slider_link_text" 
+                class="regular-text link-text"
+                value=""
+                required
+            >
+        </td>
+    </tr>
+    <tr>
+        <th>
+            <label for="wpfy_slider_link_url">Link URL</label>
+        </th>
+        <td>
+            <input 
+                type="url" 
+                name="wpfy_slider_link_url" 
+                id="wpfy_slider_link_url" 
+                class="regular-text link-url"
+                value=""
+                required
+            >
+        </td>
+    </tr>               
+</table>
+
